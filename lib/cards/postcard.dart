8fa4fce7ef71deea102caf8664ec178b73c6ec5b
yslee1126 <yslee1126@gmail.com>
2022-04-12 09:59:42 +0900

    card layout changed

--- lib/cards/postcard.dart
+++ lib/cards/postcard.dart
@@ -15,10 +15,38 @@ class _PostCardState extends State<PostCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 200,
-      color: Colors.blue,
-      child: Center(child: Text(widget.number.toString()),),
+      child: Column(
+        children: [
+          Container(
+              height: 400,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Center(child: Text('사진'),)
+          ),
+          Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.blue,
+              child: Center(child: Text('제목'),)
+          ),
+          Container(
+              height: 40,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.favorite_border), SizedBox(width: 7,), Icon(Icons.chat_outlined)
+                    ],
+                  )
+                ],
+              )
+          )
+        ],
+      ),
     );
   }
 
--- lib/mainpages/homescreen.dart
+++ lib/mainpages/homescreen.dart
@@ -11,12 +11,17 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: ListView.builder(
-      itemCount: 30, itemBuilder: (BuildContext context, int index){
-          return PostCard(
+    return Container(child: ListView.separated(
+      itemCount: 30,
+      itemBuilder: (BuildContext context, int index){
+        return PostCard(
             number : index
-          );
-        }),);
+        );
+      },
+      separatorBuilder: (BuildContext context, int index){
+        return SizedBox(height: 10,);
+      },
+    ),);
   }
 
 }
